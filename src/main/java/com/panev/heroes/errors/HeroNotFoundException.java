5b0773e945a76e50645b3ff41b4abb701b2e0044
Hristo Panev <hristopanef@gmail.com>
2019-11-24 15:25:49 +0100

    Heroes

--- src/main/java/com/panev/heroes/errors/HeroNotFoundException.java
+++ src/main/java/com/panev/heroes/errors/HeroNotFoundException.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such hero")
 public class HeroNotFoundException extends RuntimeException {
 
+
     public HeroNotFoundException(String message) {
         super(message);
     }
